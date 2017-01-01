9a738513d652c25871a4d5cb8a9f6499ad3dc07a
James T <jtara@tuta.io>
2016-12-31 21:23:46 -0800

    the log file from scrapy is now parsed returning only the list of shows

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scrapy.crawler import CrawlerProcess
 import cfscrape
 from horriblesubs_batch_downloader import HorribleSubsBatchDownloader
 import os
+import simplejson
 
 
 class HorribleSubsShowsSpider(scrapy.spiders.CrawlSpider):
@@ -32,12 +33,55 @@ class HorribleSubsShowsSpider(scrapy.spiders.CrawlSpider):
             }
 
 
+def parse_shows_from_log_file(input_file_path, expected_keys=('url', 'title'), output_file_path=None,
+                              save_to_file=False):
+    """Goes through a file and collects the dictionary from each line only containing a dictionary with keys matching
+    one of the keys from (param) expected_keys
+    :param save_to_file: if true then save the list of shows to in the output file
+    :param input_file_path: file to parse through
+    :param output_file_path: list of shows gets saved to this file
+    :param expected_keys: if a key is found in the input file that doesn't match
+    :return:
+    """
+    if save_to_file:
+        if output_file_path is None:
+            output_file_path = os.path.join(os.path.dirname(input_file_path), "shows-parsed.txt")
+        create_new_file(output_file_path)
+
+    shows = []  # list containing dictionaries
+
+    file = open(input_file_path, 'r')
+    for line in file:
+        if line.startswith('{') and "JoJo's Bizarre" not in line:
+            is_valid_show = True
+            for key in expected_keys:
+                if key not in line:
+                    is_valid_show = False
+
+            if is_valid_show:
+                line = line.replace('\'', "\"").replace(' u"', ' "').replace(" None", '""')
+                show = simplejson.loads(line)
+                shows.append(show)
+    file.close()
+
+    if save_to_file:
+        # use json to convert the list of shows to a string and write that in the output file
+        with open(output_file_path, 'w') as f:
+            f.write(simplejson.dumps(shows))
+
+    return shows
+
+
+def create_new_file(file_path):
+    if not os.path.isdir(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    with open(file_path, 'w') as f:
+        f.write('')
+
+
 def main():
     log_file_path = os.path.join(os.getcwd(), 'tmp/shows.txt')
-    if not os.path.isdir(os.path.dirname(log_file_path)):
-        os.makedirs(os.path.dirname(log_file_path))
-    with open(log_file_path, 'w') as f:
-        f.write('')
+    create_new_file(log_file_path)
 
     settings = {
         'USER_AGENT': '''Mozilla/5.0 (X11;
@@ -49,6 +93,11 @@ def main():
     process = scrapy.crawler.CrawlerProcess(settings)
     process.crawl(HorribleSubsShowsSpider)
     process.start()
+
+    # parsed_shows_file_path = os.path.join(os.path.dirname(log_file_path), "shows-parsed.txt")
+    # create_new_file(parsed_shows_file_path)
+
+    shows = parse_shows_from_log_file(log_file_path)
 
     # downloader = HorribleSubsBatchDownloader()
 
