c5741fd74f9f1c2a44d4c5cdf11cee7e19eeec16
James T <jtara@tuta.io>
2017-01-01 16:19:51 -0800

    fix show processor to properly iterate over shows & return desired_show url

--- shows_processor.py
+++ shows_processor.py
@@ -4,6 +4,8 @@ from __builtin__ import enumerate
 
 class ShowsProcessor(object):
 
+    horriblesubs_url = 'http://horriblesubs.info'
+
     def __init__(self, shows_file, search_key_word):
         """Given a list of dictionaries with keys 'name' and 'url_extension' and a search_key_word,
         determine which show the user would like
@@ -11,30 +13,30 @@ class ShowsProcessor(object):
         :param shows_file: file containing shows (list of dictionaries)
         :param search_key_word: (string) used to select a show
         """
-        self.file = open(shows_file)
+        self._file = open(shows_file)
         self.search_key_word = search_key_word
         self.matches = []  # matching shows
-        self.desired_show = None  # the show the user wants
+        self._desired_show = None  # the show the user wants
 
         self.get_matching_show()
-        self.file.close()
+        self._file.close()
 
     def get_matching_show(self):
         """Iterates through all the shows and adds each match to self.matches then determines the desired show the user
         wants"""
-        all_shows = simplejson.load(self.file)
-        # add all shows that matched search_key_word to self.matches
-        # for line in self.file:
+        all_shows = simplejson.load(self._file)
         for show in all_shows:
-            # show = simplejson.loads(line)
             if show['url_extension'] and self.search_key_word in show['url_extension']:
                 self.matches.append(show)
 
         # determine which show the user wanted
         if len(self.matches) > 1:
-            self.desired_show = self._select_a_show_from_matches()
+            self._desired_show = self._select_a_show_from_matches()
         elif len(self.matches) == 1:
-            self.desired_show = self.matches[0]
+            self._desired_show = self.matches[0]
+        else:
+            raise NoMatchingShowException(
+                "search key word, {}, not found in the list of shows".format(self.search_key_word))
 
     def _select_a_show_from_matches(self, message=""):
         """Iterates through all matching shows then asks user which show he wants"""
@@ -48,9 +50,17 @@ class ShowsProcessor(object):
         else:
             return self.matches[int(user_input)]
 
+    def get_desired_show_url(self):
+        return ShowsProcessor.horriblesubs_url + self._desired_show['url_extension']
+
+
+class NoMatchingShowException(Exception):
+    pass
+
 
 if __name__ == "__main__":
     import os
     file_path = os.path.join(os.getcwd(), 'tmp/shows.txt')
     proc = ShowsProcessor(file_path, 'jojo')
-    print(proc.desired_show)
+    print(proc._desired_show)
+    print(proc.get_desired_show_url())
